d2e5bb01fda6e5b86ac2e2d843ec4c7c0806b6be
Zak-Kent <zak.kent@gmail.com>
2022-10-06 11:10:40 -0700

    Print result of certain git commands to stdout
    This commit changes main() to print the output from commands like
    hash-object which print the hash of the object to stdout.

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,33 @@
 use std::env;
 use std::process;
 
+mod commands;
 mod config;
+mod error;
 mod objects;
 mod utils;
-mod error;
-mod commands;
 
+use crate::commands as cmd;
 use crate::config as cfg;
 use crate::error as err;
-use crate::commands as cmd;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("passed in args: {:?}", args);
 
     let cmd_config = cfg::Config::new(args, None).unwrap_or_else(|err: err::Error| {
         println!("Error: {}", err);
         process::exit(1);
     });
-    println!("Config struct: {:?}", cmd_config);
 
-    cmd::run_cmd(&cmd_config).unwrap_or_else(|err: err::Error| {
+    let output = cmd::run_cmd(&cmd_config).unwrap_or_else(|err: err::Error| {
         println!("Error: {}", err);
         process::exit(1);
     });
+
+    if let Some(out) = output {
+        println!("{}", out);
+        process::exit(0);
+    } else {
+        process::exit(0);
+    }
 }
