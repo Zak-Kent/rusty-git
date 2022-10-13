23c72c9f6f2cc006f6e5cb21e4a7c939c6603eef
Zak-Kent <zak.kent@gmail.com>
2022-10-13 12:40:02 -0700

    Add parser for key value pairs found in git objects
    This commit adds a parser which consumes the key value pairs found
    at the top of a git commit or tag object and puts those values into
    a hashmap.

--- src/object_parsers.rs
+++ src/object_parsers.rs
@@ -1,4 +1,7 @@
 use nom::branch::alt;
+use nom::bytes::complete::take_while;
+use nom::character::is_newline;
+use nom::multi::many1;
 use nom::{
     bytes::complete::{tag, take_till1},
     character::complete::space1,
@@ -6,6 +9,7 @@ use nom::{
     Err, IResult,
 };
 
+use std::collections::HashMap;
 use std::path::PathBuf;
 use std::str::from_utf8;
 
@@ -60,6 +64,23 @@ pub fn parse_git_obj<'a>(
     });
 }
 
+fn parse_kv_pair(input: &[u8]) -> IResult<&[u8], (&[u8], &[u8])> {
+    let (input, key) = take_till1(|c| c == b' ')(input)?;
+    let (input, _) = space1(input)?;
+    let (input, val) = take_till1(|c| c == b'\n')(input)?;
+    let (input, _) = take_while(is_newline)(input)?;
+    return Ok((input, (key, val)))
+}
+
+fn parse_kv_pairs(input: &[u8]) -> IResult<&[u8], HashMap<&[u8], &[u8]>> {
+    let (input, pairs) = many1(parse_kv_pair)(input)?;
+    let mut kvs = HashMap::new();
+    for (k, v) in pairs {
+        kvs.insert(k, v);
+    }
+    return Ok((input, kvs))
+}
+
 #[cfg(test)]
 mod object_parsing_tests {
     use super::*;
@@ -84,5 +105,47 @@ mod object_parsing_tests {
             obj::GitObject::Blob(PathBuf::from("foo/path")),
             gitobjinfo.obj
         );
+    }
+
+    #[test]
+    fn can_parse_kv_pair() {
+        let kv_pair = [
+            "tree".as_bytes(),
+            " ".as_bytes(),
+            "foobar".as_bytes(),
+            "\n".as_bytes(),
+        ]
+        .concat();
+        let (input, (k, v)) = parse_kv_pair(&kv_pair).unwrap();
+        assert_eq!(k, "tree".as_bytes());
+        assert_eq!(v, "foobar".as_bytes());
+        assert!(input.is_empty());
+    }
+
+    #[test]
+    fn can_parse_kv_pairs() {
+        let kv_pairs = [
+            "tree".as_bytes(),
+            " ".as_bytes(),
+            "tree-val".as_bytes(),
+            "\n".as_bytes(),
+
+            "parent".as_bytes(),
+            " ".as_bytes(),
+            "parent-val".as_bytes(),
+            "\n".as_bytes(),
+
+            "author".as_bytes(),
+            " ".as_bytes(),
+            "author val".as_bytes(),
+            "\n".as_bytes(),
+        ]
+        .concat();
+
+        let (input, pairs) = parse_kv_pairs(&kv_pairs).unwrap();
+        assert_eq!(&"tree-val".as_bytes(), pairs.get("tree".as_bytes()).unwrap());
+        assert_eq!(&"parent-val".as_bytes(), pairs.get("parent".as_bytes()).unwrap());
+        assert_eq!(&"author val".as_bytes(), pairs.get("author".as_bytes()).unwrap());
+        assert!(input.is_empty());
     }
 }
